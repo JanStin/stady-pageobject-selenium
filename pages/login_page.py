ee00b81ab9ab0d13ef892ab845afa2ae2ab19f7d
Mixail Kirilin <mixail.kirilin2012@yandex.ru>
2020-08-11 21:06:44 +0300

    Write login page class

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,20 +1,20 @@
 from .base_page import BasePage
-
+from .locators import LoginPageLocators
+import re
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
         self.should_be_login_url()
+        self.open()
         self.should_be_login_form()
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        assert True
+        result = re.findall(r"/login/", self.url)
+        assert len(result) == 1, f"Отсутствует подстрока login в данной ссылке. Полученные данные из подстроки = {result}"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Отсутствует форма входа"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "Отсутствует форма регистрации"
