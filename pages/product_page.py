06051a35acb24b5e89aa23ac3b041c8c4c982d86
Mixail Kirilin <mixail.kirilin2012@yandex.ru>
2020-08-12 13:05:00 +0300

    Added parametrize in test_product. Chaged check name product

--- pages/product_page.py
+++ pages/product_page.py
@@ -18,5 +18,4 @@ class ProductPage(BasePage):
     def should_be_quality_name(self):
         name = self.browser.find_element(*ProductPageLocators.NAME_PRODUCT).text 
         name_after = self.browser.find_element(*ProductPageLocators.NAME_PRODUCT_AFTER_ADD_MESSAGE).text 
-        result = re.findall(name, name_after)
-        assert len(result) == 1, "Разные назавания продукта. В товаре - {name}. В сообщении корзины - {name_after}"
+        assert name == name_after, "Разные назавания продукта. В товаре - {name}. В сообщении корзины - {name_after}"
